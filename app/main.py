3da5fb85c515fb74335dec32ce86b2003bf8786d
Alexis Araujo <ala1289@gmail.com>
2022-01-23 14:13:46 -0600

    Create api create new person and start and shut database poll

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from app.db import database, Person
 
 app = FastAPI(title="FastAPI, Docker, and Traefik")
 
@@ -7,6 +8,16 @@ app = FastAPI(title="FastAPI, Docker, and Traefik")
 async def read_root():
     return {"hello": "world"}
 
+@app.on_event("startup")
+async def startup_event():
+  if not database.is_connected:
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+  if database.is_connected:
+    await database.disconnect()
+
 @app.post("/person/new")
-async def create_person(name: str, age: int):
-    return {"name": name, "age": age}
+async def create_person(person: Person = Body(...)):
+    return person
