59fba81b120fd40a7a7c811a4a075cf814710b4d
Alexis Araujo <ala1289@gmail.com>
2022-01-23 12:28:23 -0600

    Request body and response body

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI(title="FastAPI, Docker, and Traefik")
 @app.get("/")
 async def read_root():
     return {"hello": "world"}
+
+@app.post("/person/new")
+async def create_person(name: str, age: int):
+    return {"name": name, "age": age}
